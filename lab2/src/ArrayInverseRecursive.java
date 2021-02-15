46038eca777c53e92a457bc1361330165509673b
Jedich <alexyudakov483@gmail.com>
2021-02-15 14:13:40 +0200

    oop fix final

--- lab2/src/ArrayInverseRecursive.java
+++ lab2/src/ArrayInverseRecursive.java
@@ -6,12 +6,12 @@ import java.util.Arrays;
 public class ArrayInverseRecursive implements Inversable {
 	@Override
 	public ArrayList<Integer> inverse(ArrayList<Integer> inputArray) {
-		return inverseRecursive(inputArray, 0, new ArrayList<>(Arrays.asList(new Integer[inputArray.size()])));
+		return inverseRecursive(inputArray, 0, new ArrayList<>());
 	}
 
 	public static ArrayList<Integer> inverseRecursive(ArrayList<Integer> inputArray, int index, ArrayList resultArray) {
 		if (index < inputArray.size()) {
-			resultArray.add(inputArray.size() - index - 1, inputArray.get(index));
+			resultArray.add(inputArray.get(inputArray.size() - index - 1));
 			inverseRecursive(inputArray, index + 1, resultArray);
 		}
 		return resultArray;
