80aea3e331272fb46f9d145c7c0152419bea6f7c
Jedich <alexyudakov483@gmail.com>
2021-02-22 12:20:58 +0200

    ordered array create implementation

--- lab4/Main.java
+++ lab4/Main.java
@@ -1,15 +1,19 @@
 public class Main {
 	public static void main(String[] args) {
 		DataProcessor dp = new DataProcessor(new BubbleSorter());
-		long time = dp.getSortTimeMillis(Operation.randomArray(10000,0,10000));
+		long time = dp.getSortTimeMillis(Operation.orderedArray(10000, Operation.Order.Asc));
 		System.out.println("BubbleSorter time: " + time);
 
 		dp = new DataProcessor(new SelectionSorter());
-		time = dp.getSortTimeMillis(new int[] {0, 3, 3, 6, 1, 9});
+		time = dp.getSortTimeMillis(Operation.randomArray(10000,0,10000));
 		System.out.println("SelectionSorter time: " + time);
 
+		dp = new DataProcessor(new InsertionSorter());
+		time = dp.getSortTimeMillis(Operation.randomArray(10000,0,10000));
+		System.out.println("InsertionSorter time: " + time);
+
 		dp = new DataProcessor(new MergeSorter());
-		time = dp.getSortTimeMillis(Operation.randomArray(25, -10, 50));
+		time = dp.getSortTimeMillis(Operation.orderedArray(10000, Operation.Order.Asc));
 		System.out.println("MergeSorter time: " + time);
 	}
 }
--- lab4/Operation.java
+++ lab4/Operation.java
@@ -1,9 +1,29 @@
 public class Operation {
+
+	public enum Order {Desc, Asc}
+
 	public static int[] randomArray(int size, int min, int max) {
 		int[] array = new int[size];
 		for (int i = 0; i < size; i++) {
 			array[i] = (int)(Math.random()*(max-min+1)+min);
 		}
 		return array;
+	}
+
+	public static int[] orderedArray(int size, Order order) {
+		int[] arr = new int[size];
+		switch (order) {
+			case Asc:
+				for (int i = 0; i < size; i++) {
+					arr[i] = i;
+				}
+				break;
+			case Desc:
+				for (int i = 0; i < size; i++) {
+					arr[(size-1) - i] = i;
+				}
+				break;
+		}
+		return arr;
 	}
 }
