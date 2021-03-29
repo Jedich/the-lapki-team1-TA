9b7becef6142d52abbaf2f60b7301b8a9cad61ec
Max <>
2021-03-29 18:04:15 +0300

    shell

--- lab5/sorters/ShellSorter.java
+++ lab5/sorters/ShellSorter.java
@@ -3,6 +3,18 @@ package sorters;
 public class ShellSorter implements Sorter {
 	@Override
 	public void sort(int[] array) {
+		int n = array.length;
 
+		for (int gap = n / 2; gap > 0; gap /= 2) {
+			for (int i = gap; i < n; i++) {
+				int key = array[i];
+				int j = i;
+				while (j >= gap && array[j - gap] > key) {
+					array[j] = array[j - gap];
+					j -= gap;
+				}
+				array[j] = key;
+			}
+		}
 	}
 }
